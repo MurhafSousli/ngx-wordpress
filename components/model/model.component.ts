c0a7444d1e885f1284e4e24b8957c46aab1554b4
Murhaf Sousli <murhafsousli@gmail.com>
2016-08-02 07:44:16 +0300

    id & args are now dynamic onchanges

--- components/model/model.component.ts
+++ components/model/model.component.ts
@@ -1,51 +1,143 @@
-import {Component, Input, Output, EventEmitter, OnInit} from '@angular/core';
+import {Injectable} from '@angular/core';
+import {Http} from '@angular/http';
+import {Observable} from "rxjs/Observable";
 
-import {WpModel} from '../../service';
+import {WpState} from "../state/state.service";
 
-@Component({
-  selector: 'model',
-  providers: [WpModel],
-  template: `<ng-content></ng-content>`
-})
+/*
+ * WpModel Service: Get/Add/Update/Delete single from WP API
+ */
 
-export class Model implements OnInit{
+@Injectable()
+export class WpModel {
 
-  /** Inputs for api endpoint, query arguments and model id */
-  @Input() endpoint:string;
-  @Input() args:any;
-  @Input() id:string;
+  public service:any;
 
-  /** Output for the response */
-  @Output() response = new EventEmitter();
-
-  private data:any;
-
-  constructor(private wpModel:WpModel) {
+  constructor(private http:Http, private state:WpState) {
   }
 
-  ngOnInit(){
-    this.fetch(this.id, this.args);
+  public Endpoint = (endpoint:string):ModelService=> {
+    if (!this.service) {
+      this.service = new ModelService(this.http, this.state, endpoint);
+    }
+    return this.service;
   }
 
-  /** Get a model of endpoint type by id */
-  public fetch(id, args?) {
-    this.wpModel.Endpoint(this.endpoint).get(id, args).subscribe(
-      (res) => {
-        this.data = res;
-        this.response.emit({object: this.data});
-      },
-      (err) => {
-        this.response.emit({error: err});
-      }
-    );
+  public Posts = ():ModelService => {
+    if (!this.service) {
+      let endpoint = '/wp-json/wp/v2/posts/';
+      this.service = new ModelService(this.http, this.state, endpoint);
+    }
+    return this.service;
+  }
+  public Users = ():ModelService=> {
+    if (!this.service) {
+      let endpoint = '/wp-json/wp/v2/users/';
+      this.service = new ModelService(this.http, this.state, endpoint);
+    }
+    return this.service;
+  }
+  public Categories = ():ModelService => {
+    if (!this.service) {
+      let endpoint = '/wp-json/wp/v2/categories/';
+      this.service = new ModelService(this.http, this.state, endpoint);
+    }
+    return this.service;
+  }
+  public Pages = ():ModelService=> {
+    if (!this.service) {
+      let endpoint = '/wp-json/wp/v2/pages/';
+      this.service = new ModelService(this.http, this.state, endpoint);
+    }
+    return this.service;
+  }
+
+  public Tags = ():ModelService=> {
+    if (!this.service) {
+      let endpoint = '/wp-json/wp/v2/tags/';
+      this.service = new ModelService(this.http, this.state, endpoint);
+    }
+    return this.service;
+  }
+
+  public Comments = ():ModelService=> {
+    if (!this.service) {
+      let endpoint = '/wp-json/wp/v2/comments/';
+      this.service = new ModelService(this.http, this.state, endpoint);
+    }
+    return this.service;
+  }
+
+  public Media = ():ModelService=> {
+    if (!this.service) {
+      let endpoint = '/wp-json/wp/v2/media/';
+      this.service = new ModelService(this.http, this.state, endpoint);
+    }
+    return this.service;
+  }
+
+  public Taxonomies = ():ModelService => {
+    if (!this.service) {
+      let endpoint = '/wp-json/wp/v2/taxonomies/';
+      this.service = new ModelService(this.http, this.state, endpoint);
+    }
+    return this.service;
+  }
+
+  public Statuses = ():ModelService=> {
+    if (!this.service) {
+      let endpoint = '/wp-json/wp/v2/statuses/';
+      this.service = new ModelService(this.http, this.state, endpoint);
+    }
+    return this.service;
+  }
+
+  public Types = ():ModelService=> {
+    if (!this.service) {
+      let endpoint = '/wp-json/wp/v2/types/'
+      this.service = new ModelService(this.http, this.state, endpoint);
+    }
+    return this.service;
   }
 
 }
 
-/*
- * Model component uses WpModel service to get a single model,
- * it has 2 properties:
- *  - @INPUT: args
- *  - @INPUT: endpoint
- *  - @OUTPUT: response
- */
+export class ModelService {
+
+  constructor(private http:Http,
+              private state:WpState,
+              private endpoint:string) {
+  }
+
+  public get = (id, args?):Observable<any> => {
+
+    return this.http.get(
+      this.state.generateUrl(this.endpoint + id, args),
+      this.state.getOptions()).map(res => res.json());
+  }
+
+  public add = (body):Observable<any> => {
+
+    return this.http.post(
+      this.state.generateUrl(this.endpoint),
+      body,
+      this.state.getOptions()).map(res => res.json());
+  }
+
+  public update = (id, body):Observable<any> => {
+
+    return this.http.put(
+      this.state.generateUrl(this.endpoint + id),
+      body,
+      this.state.getOptions()).map(res => res.json());
+  }
+
+  public delete = (id):Observable<any> => {
+
+    return this.http.delete(
+      this.state.generateUrl(this.endpoint + id + "?force=true"),
+      this.state.getOptions()).map(res => res.json());
+  }
+
+}
+
