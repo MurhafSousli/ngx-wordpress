24beb99a480e21a1091549fe61b0bbe663970897
Murhaf Sousli <murhafsousli@gmail.com>
2016-08-02 07:40:50 +0300

    constructor improvement

--- classes/args/args.model.ts
+++ classes/args/args.model.ts
@@ -1,16 +1,38 @@
 export class QueryArgs {
-  
-  public author: string;
-  public context: any;
-  public filter: any;
-  public order: string;
-  public orderby: string;
-  public page: number;
-  public per_page: number;
-  public search: string;
-  public status: any;
-  public _embed: boolean;
 
-  constructor(){
+  author:string;
+  context:any;
+  filter:any;
+  order:string;
+  orderby:string;
+  page:number;
+  per_page:number;
+  search:string;
+  status:any;
+  _embed:boolean;
+
+  constructor(options:{
+    author?:string;
+    context?:any;
+    filter?:any;
+    order?:string;
+    orderby?:string;
+    page?:number;
+    per_page?:number;
+    search?:string;
+    status?:any;
+    _embed?:boolean;
+  } = {}) {
+
+    this.author = options.author;
+    this.context = options.context;
+    this.filter = options.filter;
+    this.order = options.order;
+    this.orderby = options.orderby;
+    this.page = options.page;
+    this.per_page = options.per_page;
+    this.search = options.search;
+    this.status = options.status;
+    this._embed = options._embed;
   }
 }
