1f3bbb0433c2f36406043bfa3d3f0e5011dcb6c4
MurhafSousli <murhafsousli@gmail.com>
2017-03-05 20:22:04 +0300

    v3.0.0

--- src/service/photon/photon.service.ts
+++ src/service/photon/photon.service.ts
@@ -1,52 +1,61 @@
+import { Injectable } from '@angular/core';
 import { ConfigService } from '../config/config.service';
 import { WpPost } from '../../classes/wp-post.class';
 import { Helper } from '../../classes/helper.functions';
 
+@Injectable()
 export class PhotonService {
 
     constructor(private config: ConfigService) {
-
     }
 
-    getImage(post: WpPost, queryName?: string) {
-        if (this.config.domain && post.featuredMedia() && post.get('_embedded')) {
-
-            if (this.config.photonQueries[queryName]) {
-
-                let featuredImage = post.get('_embedded')['wp:featuredmedia'][0];
-                return 'https://i0.wp.com/' + this.config.domain + '/wp-content/uploads/' +
-                    `${featuredImage.media_details.file}?${this.config.photonQueries[queryName]}`;
-
-            } else if (this.config.domain) {
-                console.error(`[Photon]: queryName is not registered, make sure you set your query first`);
-            }
-            else if (post.featuredMedia()) {
-                console.warn(`[Photon] "${post.title().substring(0, 9)}..." does not have featured image`);
-            }
-            else {
-                console.warn(`[Photon] Your WP post object doesn't have _embedded properties `);
-            }
+    getImage(post: WpPost, photonKey: string) {
+        if (!post) {
+            console.warn('[Photon]: post is undefined');
+            return;
         }
+        if (!photonKey) {
+            console.warn('[Photon]: queryName is not registerd!');
+            return;
+        }
+        if (!this.config.photonArgs[photonKey]) {
+            console.warn(`[Photon]: "${photonKey}" is not registered, make sure the key is defined in the module`);
+            return;
+        }
+        if (!post.featuredMedia() || !post.get('_embedded')) {
+            console.warn(`[Photon]: the post "${post.title().substring(0, 9)}..." does not have featured image`);
+            return;
+        }
+
+        let photonArgs = this.config.photonArgs[photonKey];
+        let featuredImage = post.get('_embedded')['wp:featuredmedia'][0];
+        let link = 'https://i0.wp.com/' + this.config.domain + '/wp-content/uploads/' +
+            `${featuredImage.media_details.file}?${photonArgs}`;
+        return link;
+
     }
 
-    /**
+/**
      * get image using photon
      * @params {WpPost, string, any}
      */
-    getByQuery(post: WpPost, domain, queryArgs) {
-        if (domain && queryArgs && post.featuredMedia() && post.get('_embedded')) {
+    getByArgs(post: WpPost, photonArgs) {
+        if (!post) {
+            console.warn('[Photon]: post is undefined');
+            return;
+        }
+        if (!photonArgs) {
+            console.warn(`[Photon]: photonArgs is undefined`);
+            return;
+        }
+        if (!post.featuredMedia() || !post.get('_embedded')) {
+            console.warn(`[Photon]: the post "${post.title().substring(0, 9)}..." does not have featured image`);
+            return;
+        }
 
-            let query = Helper.serialize(queryArgs);
-            let featuredImage = post.get('_embedded')['wp:featuredmedia'][0];
-            return `https://i0.wp.com/${domain}/wp-content/uploads/${featuredImage.media_details.file}?${query}`;
-        } else if (this.config.domain) {
-            console.error(`[Photon]: queryName is not registered, make sure you set your query first`);
-        }
-        else if (post.featuredMedia()) {
-            console.warn(`[Photon] "${post.title().substring(0, 9)}..." does not have featured image`);
-        }
-        else {
-            console.warn(`[Photon] Your WP post object doesn't have _embedded properties `);
-        }
+        let query = Helper.serialize(photonArgs);
+        let featuredImage = post.get('_embedded')['wp:featuredmedia'][0];
+        return `https://i0.wp.com/${this.config.domain}/wp-content/uploads/${featuredImage.media_details.file}?${query}`;
     }
+
 }
