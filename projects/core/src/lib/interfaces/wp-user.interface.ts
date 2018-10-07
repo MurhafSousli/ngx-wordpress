9760dad872fe2d52188c22e10e44feb2cb0bc663
Murhaf Sousli <murhafsousli@gmail.com>
2018-10-07 22:42:32 +0300

    Update WpUser interface

--- projects/core/src/lib/interfaces/wp-user.interface.ts
+++ projects/core/src/lib/interfaces/wp-user.interface.ts
@@ -1,10 +1,20 @@
 export interface WpUser {
   id?: string | number;
+  username?: string;
   name?: string;
+  first_name?: string;
+  last_name?: string;
+  email?: string;
   url?: string;
-  link?: string;
   description?: string;
+  link?: string;
+  nickname?: string;
+  locale?: string;
   slug?: string;
+  roles?: string[];
+  registered_date?: string;
+  capabilities?: any;
+  extra_capabilities?: any;
   avatar_urls?: {
     24?: string,
     48?: string,
